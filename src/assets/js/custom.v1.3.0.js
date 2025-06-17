59a4c4cf967b6febf04e34db47555b8fecf9beaa
David <davidreypa@gmail.com>
2025-06-17 09:34:47 +0200

    Ejercicio dia 16/06

--- src/assets/js/custom.v1.3.0.js
+++ src/assets/js/custom.v1.3.0.js
@@ -1,17 +1,17 @@
 //Pedir al usuario que introduzca un número
-      let numero = prompt("Introduce un número:");
+let numero = prompt("Introduce un número:");
 
-      //Comprobamos que sea un número
-      while (isNaN(numero) || numero === null || numero === "") {
-        alert("Por favor, introduce un número válido.");
-        numero = prompt("Introduce un número:");
-      }
+//Comprobamos que sea un número
+while (isNaN(numero) || numero === null || numero === "") {
+  alert("Por favor, introduce un número válido.");
+  numero = prompt("Introduce un número:");
+}
 
-      //Comprobar si el número es par o impar
-      if (numero % 2 === 0) {
-        console.log("El número " + numero + " es par.");
-        alert("El número " + numero + " es par.");
-      } else {
-        console.log("El número " + numero + " es impar.");
-        alert("El número " + numero + " es impar.");
-      }
+//Comprobar si el número es par o impar
+if (numero % 2 === 0) {
+  console.log("El número " + numero + " es par.");
+  alert("El número " + numero + " es par.");
+} else {
+  console.log("El número " + numero + " es impar.");
+  alert("El número " + numero + " es impar.");
+}
