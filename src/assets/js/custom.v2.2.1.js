20449a060b372a0a2280d0cb327e623798845eea
David <davidreypa@gmail.com>
2025-06-24 10:22:02 +0200

    Mejora Fibonacci

--- src/assets/js/custom.v2.2.1.js
+++ src/assets/js/custom.v2.2.1.js
@@ -1,12 +1,20 @@
+//Creamos un parrafo donde pongamos fibonacci
+let parrFibonacci = document.createElement("p");
+parrFibonacci.id = "parrafoFibonacci"; //Le ponemos un ID al parrafo
+parrFibonacci.textContent = "Secuencia Fibonacci actual:"; //Le añadimos contenido al parrafo
+
+//Añadimos este parrafo a la seccion de parrafos
+document.getElementById("seccionParrafos")?.appendChild(parrFibonacci);
+
 //Creamos un boton para aumentar el contador
 let botonFibonacci = document.createElement("button");
 botonFibonacci.id = "fibonacciContador"; //Le ponemos un ID al botón
 botonFibonacci.textContent = "Contador Fibonacci "; //Le añadimos contenido al botón
 
-//Añadimos estos botónes al final de la sección
+//Añadimos este botón al final de la sección
 document.getElementById("seccionBotones")?.appendChild(botonFibonacci);
 
-//Seleccionamos el texto del parrafo (y le añadimo un espacio al final)
+//Seleccionamos el texto del parrafo (y le añadimos un espacio al final)
 let textoParrafo =
   document.getElementById("parrafoFibonacci").textContent + " ";
 
