08b6be1ac39aa619081a092ab4fcbe7305d30b2f
David <davidreypa@gmail.com>
2025-06-24 12:06:15 +0200

    Update custom.v2.3.0.js

--- src/assets/js/custom.v2.3.0.js
+++ src/assets/js/custom.v2.3.0.js
@@ -7,24 +7,39 @@ const parrafoVisibilidad = document.getElementById("parrafoPrincipal");
 const contenedor = parrafoVisibilidad.parentNode;
 contenedor.replaceChild(divVisibilidad, parrafoVisibilidad);
 divVisibilidad.appendChild(parrafoVisibilidad);
-// document.getElementById("seccionParrafos")?.appendChild(divVisibilidad);
 
-//Creamos un boton para ocultar o enseñar el contenido
+//Creamos un boton para ocultar o enseñar el contenido 
 let botonVisibilidad = document.createElement("button");
 botonVisibilidad.id = "botonVisibilidad"; //Le ponemos un ID al botón
 botonVisibilidad.textContent = "Ocultar/Mostar párrafo "; //Le añadimos contenido al botón
 
-//Añadimos este botón al final de la sección
+//Creamos un boton para ocultar o enseñar el contenido en modo acordeon
+let botonAcordeon = document.createElement("button");
+botonAcordeon.id = "botonAcordeon"; //Le ponemos un ID al botón
+botonAcordeon.textContent = "Ocultar/Mostar párrafo acordeon "; //Le añadimos contenido al botón
+
+//Añadimos estos botónes al final de la sección
 document.getElementById("seccionBotones")?.appendChild(botonVisibilidad);
+document.getElementById("seccionBotones")?.appendChild(botonAcordeon);
+
+let visibilidad = document.getElementById("divVisibilidad");
 
 function ocultarParrafo() {
-  let visibilidad = document.getElementById("divVisibilidad").style.display;
   //Si la visibilidad está en oculta, la enseña (en el primer caso no esta ni mostrando ni ocultando, por lo que entra en el else y se oculta)
-  if (visibilidad == "none") {
+  if (visibilidad.style.display == "none") {
     document.getElementById("divVisibilidad").style.display = "block";
   } else {
     document.getElementById("divVisibilidad").style.display = "none";
   }
 }
 
+function ocultarAcordeon() {
+  if (visibilidad.style.visibility == "hidden") {
+    document.getElementById("divVisibilidad").style.visibility = "visible";
+  } else {
+    document.getElementById("divVisibilidad").style.visibility = "hidden";
+  }
+}
+
 botonVisibilidad.onclick = ocultarParrafo;
+botonAcordeon.onclick = ocultarAcordeon;
