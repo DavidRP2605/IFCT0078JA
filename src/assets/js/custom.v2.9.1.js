ce9c3afac20c7899f9b91cbe352fb33103b99571
David <davidreypa@gmail.com>
2025-07-02 10:35:18 +0200

    Version tabla con una td vacia para insertar el error

--- src/assets/js/custom.v2.9.1.js
+++ src/assets/js/custom.v2.9.1.js
@@ -17,9 +17,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //Creamos funcion para comprobar que el campo nombre no este vacio y tenga al menos 3 caracteres y lo añadimos en un parrafo que se añade debajo del campo nombre solo si no es correcto
   function validarNombre() {
-    let trNombre = document.getElementById("filaNombre");
     let parrafoError = document.getElementById("errorNombre");
-    let tdError = document.getElementById("tdNombre");
+    let tdNombre = document.getElementById("tdNombre");
     //Guardamos el largo del nombre para comprobarlo
     const largoNombre = inputNombre.value.length;
     //Guardamos la variable por si el nombre está vacío
@@ -31,13 +30,11 @@ document.addEventListener("DOMContentLoaded", () => {
       !/^[a-zA-ZÀ-ÿ\s]+$/.test(inputNombre.value.trim())
     ) {
       if (!parrafoError) {
-        tdError = document.createElement("td");
         parrafoError = document.createElement("p");
-        tdError.id = "errorNombre";
+        parrafoError.id = "errorNombre";
         parrafoError.textContent = "El nombre introducido no es válido.";
         parrafoError.style.color = "red";
-        trNombre.appendChild(tdError);
-        tdError.appendChild(parrafoError);
+        tdNombre.appendChild(parrafoError);
       }
       return false;
     } else {
@@ -53,9 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //Creamos funcion para comprobar que el campo apellidos no este vacio y tenga al menos 3 caracteres y lo añadimos en un parrafo que se añade debajo del campo apellidos solo si no es correcto
   function validarApellidos() {
     let parrafoError = document.getElementById("errorApellidos");
-    let trApellidos = document.getElementById("filaApellidos");
-    let tdError = document.getElementById("tdApellidos");
-
+    let tdApellidos = document.getElementById("tdApellidos");
     //Guardamos el largo del nombre para comprobarlo
     const largoApellidos = inputApellidos.value.length;
     //Guardamos la variable por si el nombre está vacío
@@ -67,13 +62,11 @@ document.addEventListener("DOMContentLoaded", () => {
       !/^[a-zA-ZÀ-ÿ\s]+$/.test(inputApellidos.value.trim())
     ) {
       if (!parrafoError) {
-        tdError = document.createElement("td");
         parrafoError = document.createElement("p");
-        tdError.id = "errorApellidos";
+        parrafoError.id = "errorApellidos";
         parrafoError.textContent = "El apellido introducido no es válido.";
         parrafoError.style.color = "red";
-        trApellidos.appendChild(tdError);
-        tdError.appendChild(parrafoError);
+        tdApellidos.appendChild(parrafoError);
       }
       return false;
     } else {
@@ -103,8 +96,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function validarFechaNacimiento() {
     let parrafoError = document.getElementById("errorFecha");
-    let trEdad = document.getElementById("filaEdad");
-    let tdError = document.getElementById("tdEdad");
+    let tdEdad = document.getElementById("tdEdad");
 
     const dia = parseInt(inputNacimiento.value.split("-")[2]);
     const mes = parseInt(inputNacimiento.value.split("-")[1]);
@@ -114,13 +106,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (!edad || edad < 18) {
       if (!parrafoError) {
-        tdError = document.createElement("td");
         parrafoError = document.createElement("p");
-        tdError.id = "errorFecha";
-        parrafoError.textContent = "La edad no es válida.";
+        parrafoError.id = "errorFecha";
+        parrafoError.textContent = "Edad inválida";
         parrafoError.style.color = "red";
-        trEdad.appendChild(tdError);
-        tdError.appendChild(parrafoError);        
+        tdEdad.appendChild(parrafoError);
       }
       return false;
     } else {
@@ -137,19 +127,16 @@ document.addEventListener("DOMContentLoaded", () => {
   //Creamos funcion para comprobar que el campo email no este vacio y tenga estructura de email y lo añadimos a la derecha del campo email solo si no es correcto
   function validarEmail() {
     let parrafoError = document.getElementById("errorEmail");
-    let trEmail = document.getElementById("filaEmail");
-    let tdError = document.getElementById("tdEmail");
+    let tdEmail = document.getElementById("tdEmail");
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(inputEmail.value)) {
       if (!parrafoError) {
-        tdError = document.createElement("td");
         parrafoError = document.createElement("p");
-        tdError.id = "errorEmail";
+        parrafoError.id = "errorEmail";
         parrafoError.textContent = "El email no es válido.";
         parrafoError.style.color = "red";
-        trEmail.appendChild(tdError);
-        tdError.appendChild(parrafoError);        
+        tdEmail.appendChild(parrafoError);
       }
       return false;
     } else {
@@ -166,18 +153,15 @@ document.addEventListener("DOMContentLoaded", () => {
   function validarProvincia() {
     let provinciaVacia = inputProvincia.value;
     let parrafoError = document.getElementById("errorProvincia");
-    let trProvincia = document.getElementById("filaProvincia");
-    let tdError = document.getElementById("tdProvincia");
+    let tdProvincia = document.getElementById("tdProvincia");
 
     if (provinciaVacia === "selecciona") {
       if (!parrafoError) {
-        tdError = document.createElement("td");
         parrafoError = document.createElement("p");
-        tdError.id = "errorProvincia";
+        parrafoError.id = "errorProvincia";
         parrafoError.textContent = "La provincia no es válida.";
         parrafoError.style.color = "red";
-        trProvincia.appendChild(tdError);
-        tdError.appendChild(parrafoError);        
+        tdProvincia.appendChild(parrafoError);
       }
       return false;
     } else {
@@ -196,18 +180,15 @@ document.addEventListener("DOMContentLoaded", () => {
     );
 
     let parrafoError = document.getElementById("errorTecnologias");
-    let trTecnologias = document.getElementById("filaTecnologias");
-    let tdError = document.getElementById("tdTecnologias");
+    let tdTecnologias = document.getElementById("tdTecnologias");
 
     if (seleccionadas.length === 0) {
       if (!parrafoError) {
-        tdError = document.createElement("td");
         parrafoError = document.createElement("p");
-        tdError.id = "errorTecnologias";
+        parrafoError.id = "errorTecnologias";
         parrafoError.textContent = "Debes seleccionar al menos 1 tecnología.";
         parrafoError.style.color = "red";
-        trTecnologias.appendChild(tdError);
-        tdError.appendChild(parrafoError);        
+        tdTecnologias.appendChild(parrafoError);
       }
       return false;
     } else {
@@ -223,9 +204,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //Creamos funcion para validar que el campo experiencia no esta vacío ni introduce número menores que 0 o mayores que 50
   function validarExperiencia() {
     let parrafoError = document.getElementById("errorExperiencia");
-    let trExperiencia = document.getElementById("filaExperiencia");
-    let tdError = document.getElementById("tdExperiencia");
-
+    let tdExperiencia = document.getElementById("tdExperiencia");
     let valor = inputExperiencia.value;
 
     //Guardamos la variable por si la experiencia está vacía
@@ -238,13 +217,11 @@ document.addEventListener("DOMContentLoaded", () => {
       valor > 50
     ) {
       if (!parrafoError) {
-        tdError = document.createElement("td");
         parrafoError = document.createElement("p");
-        tdError.id = "errorExperiencia";
+        parrafoError.id = "errorExperiencia";
         parrafoError.textContent = "La experiencia no es válida.";
         parrafoError.style.color = "red";
-        trExperiencia.appendChild(tdError);
-        tdError.appendChild(parrafoError);        
+        tdExperiencia.appendChild(parrafoError);
       }
       return false;
     } else {
@@ -260,8 +237,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function validarCV() {
     const inputCv = document.getElementById("campoCV");
     let parrafoError = document.getElementById("errorCv");
-    let trCv = document.getElementById("filaCv");
-    let tdError = document.getElementById("tdCv");
+    let tdCv = document.getElementById("tdCv");
 
     const archivo = inputCv.files[0];
 
@@ -269,11 +245,10 @@ document.addEventListener("DOMContentLoaded", () => {
       if (!parrafoError) {
         tdError = document.createElement("td");
         parrafoError = document.createElement("p");
-        tdError.id = "errorCv";
+        parrafoError.id = "errorCv";
         parrafoError.textContent = "Debes subir algún archivo.";
         parrafoError.style.color = "red";
-        trCv.appendChild(tdError);
-        tdError.appendChild(parrafoError);        
+        tdCv.appendChild(parrafoError);
       }
       return false;
     } else {
@@ -286,19 +261,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function validarCheckboxes() {
     let parrafoError = document.getElementById("errorCheckbox");
-    let trCheckbox = document.getElementById("filaCheckbox");
-    let tdError = document.getElementById("tdCheckbox");
+    let tdCheckbox = document.getElementById("tdCheckbox");
 
     if (!checkPolitica.checked || !checkCondiciones.checked) {
       if (!parrafoError) {
-        tdError = document.createElement("td");
         parrafoError = document.createElement("p");
-        tdError.id = "errorCheckbox";
+        parrafoError.id = "errorCheckbox";
         parrafoError.textContent =
           "Debes aceptar la política de privacidad y las condiciones de uso.";
         parrafoError.style.color = "red";
-        trCheckbox.appendChild(tdError);
-        tdError.appendChild(parrafoError);        
+        tdCheckbox.appendChild(parrafoError);
       }
       return false;
     } else {
